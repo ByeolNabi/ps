dc3d89e348f4463d756fac5dff1dd2ac5513508c
ByeolNabi <20183338@office.deu.ac.kr>
2023-10-23 15:41:28 +0900

    [sovled] boj 10989 >> 해싱, count sort?

--- boj/10989.cpp
+++ boj/10989.cpp
@@ -3,24 +3,31 @@
 #include<queue>
 using namespace std;
 
+int arr[10001];
 int main(){
-    priority_queue<int, vector<int>, greater<int>> q;
     int tc;
     scanf("%d", &tc);
-    int data;
     while(tc--){
-        scanf("%d", &data);
-        q.push(data);
+        int number;
+        scanf("%d", &number);
+        arr[number]++;
     }
-    int sz = q.size();
-    while(sz--){
-        printf("%d\n", q.top());
-        q.pop();
+    for(int i = 1; i < 10001; ++i){
+        if(arr[i]==0){
+            continue;
+        }
+        else{
+            for(int j = 0; j < arr[i]; ++j){
+               printf("%d\n",i);
+            }
+        }
     }
+    
     return 0;
 }
 
 /*
 메모리 초과 : 천만개를 다 저장해서 메모리 초과가 났다.
 해싱을 이용해서 저장하자
+틀림: short를 이용해서 오버플로우가 났다.
 */
