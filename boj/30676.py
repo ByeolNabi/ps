650bbd724462a19f406579df91849efa244dc2db
ByeolNabi <narwhal_blast@naver.com>
2024-09-29 23:32:34 +0800

    [백준] 브론즈 문제 풀이

---
+++ boj/30676.py
@@ -0,0 +1,18 @@
+# 이 별은 무슨 색일까 [브론즈5]
+
+numb = int(input())
+
+if 620 <= numb:
+    print('Red')
+elif 590 <= numb:
+    print('Orange')
+elif 570 <= numb:
+    print('Yellow')
+elif 495 <= numb:
+    print('Green')
+elif 450 <= numb:
+    print('Blue')
+elif 425 <= numb:
+    print('Indigo')
+elif 380 <= numb:
+    print('Violet')
