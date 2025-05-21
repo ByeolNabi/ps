4d0880a3dbdab903153c17aaeb7f62c7fc74fa01
ByeolNabi <narwhal_blast@naver.com>
2025-05-22 03:14:15 +0900

    [해결] LCS + 문자열 복원

--- boj/09252.cpp
+++ boj/09252.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int dp[1001][1001];
 
+string dfs(string str, int row, int col) {
+  // 바닥 조건
+  if (dp[row][col] == 0) {
+    return "";
+  }
+  // 일반 조건
+  if (dp[row][col] == dp[row - 1][col]) {
+    return dfs(str, row - 1, col);
+  } else if (dp[row][col] == dp[row][col - 1]) {
+    return dfs(str, row, col - 1);
+  }
+  // cout << row << " : " << col << endl;
+  return dfs(str, row - 1, col - 1) + str[col - 1];
+}
+
 int main() {
   string top, left;
   cin >> top >> left;
@@ -18,11 +33,16 @@ int main() {
     }
   }
 
-  for (int i = 0; i <= left.length(); i++) {
-    for (int j = 0; j <= top.length(); j++) {
-      printf("%d ", dp[i][j]);
-    }
-    printf("\n");
+  // for (int i = 0; i <= left.length(); i++) {
+  //   for (int j = 0; j <= top.length(); j++) {
+  //     printf("%d ", dp[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+
+  cout << dp[left.length()][top.length()] << "\n";
+  if (dp[left.length()][top.length()] != 0) {
+    cout << dfs(top, left.length(), top.length()) << "\n";
   }
 
   return 0;
@@ -30,5 +50,13 @@ int main() {
 
 /*
 복원 알고리즘을 어떻게 해야할까?
-=>
+=> BFS!! 모서리 숫자 찾기, 찾으면 -1,-1방향으로 이동
+0 A C A Y K P
+C 0 1 1 1 1 1
+A 1 1 2 2 2 2
+P 1 1 2 2 2 3
+C 1 2 2 2 2 3
+A 1 2 3 3 3 3
+K 1 2 3 3 4 4
+
 */
